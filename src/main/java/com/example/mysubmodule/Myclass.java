26954ad46a0cf220b9e81171978e4bf8329bc4b6
Max Nasalevich <maks20061999@gmail.com>
2019-02-23 22:04:50 +0300

    add submodule

---
+++ src/main/java/com/example/mysubmodule/Myclass.java
@@ -0,0 +1,10 @@
+package com.example.mysubmodule;
+
+import android.util.Log;
+
+public class Myclass {
+    private static final String TAG = "MyClass";
+    public static void myFunc(){
+        Log.d(TAG, "myFunc() called");
+    }
+}
